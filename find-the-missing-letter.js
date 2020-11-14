a23f97babb89a4a00fd422d54ad2e120ad3ad3f6
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-14 17:17:29 -0600

    obtain diiffrent

--- find-the-missing-letter.js
+++ find-the-missing-letter.js
@@ -1,29 +1,67 @@
-// findMissingLetter(['a', 'b', 'c', 'd', 'f']) // -> 'e'
-findMissingLetter(['O','Q','R','S'])// -> 'P'
+findMissingLetter(['a', 'b', 'c', 'd', 'f']) // -> 'e'
+findMissingLetter(['O', 'Q', 'R', 'S']) // -> 'P'
 
 function findMissingLetter(array) {
-  const min = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s" ,"u", "v", "w", "x", "y", "z"];
+  const min = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "u", "v", "w", "x", "y", "z"];
   const mai = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "U", "V", "W", "X", "Y", "Z"];
   let firstLetter = array[0]
-  let segment = []
+  let correctsegment = []
   let indexOfcoincidece = null
   let times = array.length;
+
   mai.forEach((element, index) => {
     //obtener indice donde 1era letra y elemento de abecedario coinciden
     if (element == firstLetter) {
       indexOfcoincidece = index
     }
     //hacer nuevo array apartir de ese indice, dicho array es de la longitud del array input
-    if (indexOfcoincidece) {
-        times--
-        if (times >= 0) {
-          segment.push(element)
-
-        }
+    if (indexOfcoincidece != null) {
+      times--
+      if (times >= 0) {
+        correctsegment.push(element)
+      }
     }
-
   });
 
-  console.log(segment);
-  return ' ';
+  min.forEach((element, index) => {
+    //obtener indice donde 1era letra y elemento de abecedario coinciden
+    if (element == firstLetter) {
+      indexOfcoincidece = index
+    }
+    //hacer nuevo array apartir de ese indice, dicho array es de la longitud del array input
+    if (indexOfcoincidece != null) {
+      times--
+      if (times >= 0) {
+        correctsegment.push(element)
+      }
+    }
+  });
+
+  function arr_diff(a1, a2) {
+
+    let a = [],
+      diff = [];
+
+    for (let i = 0; i < a1.length; i++) {
+      a[a1[i]] = true;
+    }
+
+    for (let i = 0; i < a2.length; i++) {
+      if (a[a2[i]]) {
+        delete a[a2[i]];
+      } else {
+        a[a2[i]] = true;
+      }
+    }
+
+    for (let k in a) {
+      diff.push(k);
+    }
+
+    return diff;
+  }
+
+  let results = arr_diff(array,correctsegment)
+
+  console.log(results[results.length-1]);
 }
