4c36c463a4f2a3155457f6be272b624f9e85e29e
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-14 17:38:30 -0600

    get V

--- find-the-missing-letter.js
+++ find-the-missing-letter.js
@@ -1,9 +1,10 @@
-findMissingLetter(['a', 'b', 'c', 'd', 'f']) // -> 'e'
-findMissingLetter(['O', 'Q', 'R', 'S']) // -> 'P'
+// findMissingLetter(['a', 'b', 'c', 'd', 'f']) // -> 'e'
+// findMissingLetter(['O', 'Q', 'R', 'S']) // -> 'P'
+findMissingLetter(["T","U","W"]) // -> 'V'
 
 function findMissingLetter(array) {
-  const min = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "u", "v", "w", "x", "y", "z"];
-  const mai = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "U", "V", "W", "X", "Y", "Z"];
+  const min = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"];
+  const mai = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   let firstLetter = array[0]
   let correctsegment = []
   let indexOfcoincidece = null
