4f3f506f973899e653dd094bb8d92dfbb7184266
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-14 16:38:49 -0600

    get segment

--- find-the-missing-letter.js
+++ find-the-missing-letter.js
@@ -1,23 +1,29 @@
-findMissingLetter(['a', 'b', 'c', 'd', 'f']) // -> 'e'
+// findMissingLetter(['a', 'b', 'c', 'd', 'f']) // -> 'e'
 findMissingLetter(['O','Q','R','S'])// -> 'P'
 
 function findMissingLetter(array) {
   const min = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s" ,"u", "v", "w", "x", "y", "z"];
   const mai = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "U", "V", "W", "X", "Y", "Z"];
   let firstLetter = array[0]
-  //encontrar la 1era letra del array en min o en mai
-  min.forEach((element, index) => {
-    if (element == firstLetter) {
-      console.log("es min",element);
-    }
-  });
+  let segment = []
+  let indexOfcoincidece = null
+  let times = array.length;
   mai.forEach((element, index) => {
+    //obtener indice donde 1era letra y elemento de abecedario coinciden
     if (element == firstLetter) {
-      console.log("es may",element);
+      indexOfcoincidece = index
     }
+    //hacer nuevo array apartir de ese indice, dicho array es de la longitud del array input
+    if (indexOfcoincidece) {
+        times--
+        if (times >= 0) {
+          segment.push(element)
+
+        }
+    }
+
   });
 
-
-  // console.log(min.splice(0,array.length));
+  console.log(segment);
   return ' ';
 }
